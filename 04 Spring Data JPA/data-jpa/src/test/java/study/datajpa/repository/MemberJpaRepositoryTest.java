e19accbdf12fb82cd09231d8e81ac734c97eabdf
황도영 <ghkdehdud96@naver.com>
2023-03-15 01:38:28 +0900

    스프링데이터 jpa 0315

--- 04 Spring Data JPA/data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ 04 Spring Data JPA/data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -8,6 +8,8 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -29,7 +31,32 @@ class MemberJpaRepositoryTest {
         assertThat(findMember.getId()).isEqualTo(member.getId());
         assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
         assertThat(findMember).isEqualTo(member);
+    }
 
+    @Test
+    public void basicCrud() {
+        Member member1 = new Member("kim");
+        Member member2 = new Member("kim2");
+
+        memberJpaRepository.save(member1);
+        memberJpaRepository.save(member2);
+
+        Member findmember1 = memberJpaRepository.findById(member1.getId()).get();
+        Member findmember2= memberJpaRepository.findById(member2.getId()).get();
+        assertThat(findmember1).isEqualTo(member1);
+        assertThat(findmember2).isEqualTo(member2);
+
+        List<Member> all = memberJpaRepository.findAll();
+        assertThat(all.size()).isEqualTo(2);
+
+        long count = memberJpaRepository.count();
+        assertThat(count).isEqualTo(2);
+
+        memberJpaRepository.delete(member1);
+        memberJpaRepository.delete(member2);
+
+        long deleteCount = memberJpaRepository.count();
+        assertThat(deleteCount).isEqualTo(0);
 
     }
 }
--- 04 Spring Data JPA/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ 04 Spring Data JPA/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -7,6 +7,8 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -27,7 +29,31 @@ class MemberRepositoryTest {
         assertThat(findMember.getId()).isEqualTo(member.getId());
         assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
         assertThat(findMember).isEqualTo(member);
+    }
 
+    @Test
+    public void basicCrud() {
+        Member member1 = new Member("kim");
+        Member member2 = new Member("kim2");
 
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        Member findmember1 = memberRepository.findById(member1.getId()).get();
+        Member findmember2= memberRepository.findById(member2.getId()).get();
+        assertThat(findmember1).isEqualTo(member1);
+        assertThat(findmember2).isEqualTo(member2);
+
+        List<Member> all = memberRepository.findAll();
+        assertThat(all.size()).isEqualTo(2);
+
+        long count = memberRepository.count();
+        assertThat(count).isEqualTo(2);
+
+        memberRepository.delete(member1);
+        memberRepository.delete(member2);
+
+        long deleteCount = memberRepository.count();
+        assertThat(deleteCount).isEqualTo(0);
     }
 }
